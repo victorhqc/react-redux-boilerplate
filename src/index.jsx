e1e28e13878cb3ffd159e5f0485b285fd97d00d7
Victor Quiroz <victorhqc@gmail.com>
2016-09-06 20:57:39 +0200

    Saving State into Local Storage

--- src/index.jsx
+++ src/index.jsx
@@ -2,25 +2,39 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { Router, Route, browserHistory } from 'react-router';
+import throttle from 'lodash/throttle';
 
 // Redux dependencies
 import { Provider } from 'react-redux';
 import { syncHistoryWithStore } from 'react-router-redux';
 
+// Material UI dependencies
+import injectTapEventPlugin from 'react-tap-event-plugin';
+
 import configureStore from './store/configureStore';
 
 import Root from './components/core/Root';
 
+import { loadState, saveState } from './helpers/localStorage';
+
 require('roboto-fontface/css/roboto-fontface.css');
 
-const store = configureStore();
+const store = configureStore(loadState());
+
+store.subscribe(throttle(() => {
+    saveState(store.getState());
+}, 500));
+
+injectTapEventPlugin();
 
 // Create an enhanced history that syncs navigation events with the store
 const history = syncHistoryWithStore(browserHistory, store);
 
+/* global document:true */
+/* eslint no-undef: "error" */
+
 render(
     <Provider store={store}>
-        { /* Tell the Router to use our enhanced history */ }
         <Router history={history}>
             <Route path="/" component={Root} />
         </Router>
