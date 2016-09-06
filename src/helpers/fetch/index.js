a4231c254c2b3d9ef03d4fbec0a2ad65e5d3447b
Victor Quiroz <victorhqc@gmail.com>
2016-09-06 22:43:03 +0200

    Adding jsonHeaders function
    To the fetch/helper file

--- src/helpers/fetch/index.js
+++ src/helpers/fetch/index.js
@@ -3,6 +3,15 @@ export const JSON_HEADERS = {
     'Content-Type': 'application/json',
 };
 
+export function jsonHeaders(token) {
+    const headers = JSON_HEADERS;
+    if (token) {
+        headers.Authorization = token;
+    }
+
+    return headers;
+}
+
 export function checkStatus(response) {
     if (response.status >= 200 && response.status < 300) {
         return response;
