d1de86540c8f2a43be0d6c278ea6ac106b6a9965
Victor Quiroz <victorhqc@gmail.com>
2016-09-06 16:20:17 +0200

    Changes in bootstrap

--- src/components/core/Root.local.jsx
+++ src/components/core/Root.local.jsx
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
+// Material UI Dependencies
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
 import DevTools from './DevTools';
 
@@ -6,8 +9,13 @@ import DevTools from './DevTools';
 export default class Root extends Component {
 
     render() {
+        const props = this.props;
+
         return (
             <div>
+                <MuiThemeProvider muiTheme={getMuiTheme()}>
+                    {props.children}
+                </MuiThemeProvider>
                 <DevTools />
             </div>
         );
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,7 @@
 import { combineReducers } from 'redux';
 import { routerReducer } from 'react-router-redux';
 
+/* eslint-disable import/prefer-default-export */
 export const rootReducer = combineReducers({
+    routing: routerReducer,
 });
